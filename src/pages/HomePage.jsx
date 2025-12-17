b0ce69218e44e11790b781b69ce646837e98c996
LucaStellato <lucastellato435@gmail.com>
2025-12-18 00:16:45 +0100

    add thead table

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,9 +12,23 @@ export default function HomePage() {
         <>
             <h1>Aggiungere la lista dei viaggi qui!!</h1>
             {//faccio un map dei viaggi per stamparli a schermo e controllare se i dati vengono aquisiti
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Destinazione</th>
+                            <th scope="col">Data Partenza</th>
+                            <th scope="col">Data Ritorno</th>
+                        </tr>
+                        <tbody>
+
+                        </tbody>
+                    </thead>
+                </table>
+                
+                
                 travel.map(el => (
-                    <h2 key={el.id}>{el.destinazione}</h2>
-                ))
+            <h2 key={el.id}>{el.destinazione}</h2>
+            ))
             }
         </>
     )
